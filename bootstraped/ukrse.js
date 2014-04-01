6f39ad0a08a8f51118ce878cc5de62f54c8f7e11
Bartosz Szczesny <bszcz@bszcz.org>
2014-04-01 19:12:33 +0100

    smooth scrolling

---
+++ bootstraped/ukrse.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 700);
+  });
+});
+
+
