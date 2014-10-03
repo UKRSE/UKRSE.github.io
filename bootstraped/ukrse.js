1c0f5e3aaaf5fb0799cf2050431d3d8403c08794
Bartosz Szczesny <bszcz@bszcz.org>
2014-10-03 19:30:26 +0100

    fix whitespace

--- bootstraped/ukrse.js
+++ bootstraped/ukrse.js
@@ -1,10 +1,8 @@
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      $('html,body').animate({
-        scrollTop: target.offset().top
-      }, 500);
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    $('html,body').animate({scrollTop: target.offset().top}, 500);
   });
 });
 
